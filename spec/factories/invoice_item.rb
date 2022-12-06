4dac9272b0ca0803e3ef718395ff576761715f80
Darby Smith <newman.darby2@gmail.com>
2022-12-06 13:36:56 -0500

    Item show endpoint

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :invoice_item do
-    quantity { Faker::Number.within_range(1..20) }
+    quantity { Faker::Number.within(range: 1..20) }
     unit_price { Faker::Number.decimal(l_digits: 2) }
     item
     invoice
