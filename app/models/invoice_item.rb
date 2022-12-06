794f130bb1fdf734e513f6a07f1999ae8cd21e22
Darby Smith <newman.darby2@gmail.com>
2022-12-05 22:54:26 -0500

    Relationships and validations

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,7 @@
 class InvoiceItem < ApplicationRecord
-  
+  belongs_to :item
+  belongs_to :invoice
+  has_one :merchant, through: :item
+
+  validates_presence_of :quantity, :unit_price, :item_id, :invoice_id
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
+  has_one :merchant, through: :invoice
 
+  validates_presence_of :credit_card_number, :result, :invoice_id
 end
