e5ff6c6a9d352cf8317898a70cfd5564d1f045cb
Darby Smith <newman.darby2@gmail.com>
2022-12-05 22:56:22 -0500

    WIP

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
-    item { Faker::Lorem.word }
+    name { Faker::Lorem.word }
     description { Faker::Lorem.paragraph }
     unit_price { Faker::Number.decimal(l_digits: 2) }
-    merchant_id { Faker::Numer.within_range(1..100) }
+    merchant
   end
 end
