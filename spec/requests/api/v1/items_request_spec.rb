676c35c6bd8819452f6ef85ba5a7a35d2dbb4175
Darby Smith <newman.darby2@gmail.com>
2022-12-05 23:04:30 -0500

    Items index endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,16 +13,16 @@ RSpec.describe 'Items API' do
 
     items.each do |item|
       expect(item).to have_key(:name)
-      expect(item.name).to be_a(String)
+      expect(item[:name]).to be_a(String)
 
       expect(item).to have_key(:description)
-      expect(item.description).to be_a(String)
+      expect(item[:description]).to be_a(String)
 
       expect(item).to have_key(:unit_price)
-      expect(item.unit_price).to be_a(Float)
+      expect(item[:unit_price]).to be_a(Float)
 
       expect(item).to have_key(:merchant_id)
-      expect(item.merchant_id).to be_an(Integer)
+      expect(item[:merchant_id]).to be_an(Integer)
     end
   end
 end
