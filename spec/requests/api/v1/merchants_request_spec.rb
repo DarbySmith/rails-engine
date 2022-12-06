ad69121fff2ac46666155476aaa80d12e6ab60bc
Darby Smith <newman.darby2@gmail.com>
2022-12-05 21:24:26 -0500

    Merchant show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe 'Merchants API' do
       expect(merchant[:name]).to be_a(String)
     end
   end
+
+  it 'can get merchant by its id' do
+    id = create(:merchant).id
+    
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)    
+  end
 end
