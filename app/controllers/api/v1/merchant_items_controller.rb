82385411ff854d4637d0d2ed53b153e1e8994537
Darby Smith <newman.darby2@gmail.com>
2022-12-07 12:59:07 -0500

    Make merchant items more restful

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::MerchantItemsController < ApplicationController
+  def index
+    if Merchant.exists?(params[:merchant_id])
+      render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
+    else
+      render json: {error: "unable to find" }, status: 404
+    end
+  end
+end
