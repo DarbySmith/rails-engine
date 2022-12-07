2d2a4806ebdab18a58bd111859771c63a28431af
Darby Smith <newman.darby2@gmail.com>
2022-12-07 16:47:07 -0500

    Sad path testing item merchant

--- spec/requests/api/v1/item_merchant_spec.rb
+++ spec/requests/api/v1/item_merchant_spec.rb
@@ -14,4 +14,10 @@ RSpec.describe "Item merchant API endpoint" do
     expect(merchant[:attributes]).to have_key(:name)
     expect(merchant[:attributes][:name]).to be_a(String)
   end
+
+  it 'returns a 400 error when the item does not exist' do
+    get "/api/v1/items/1/merchant"
+
+    expect(response).to have_http_status(404)
+  end
 end
